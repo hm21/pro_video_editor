439753ad46be476759f19a11208b6f9b118c4e8e
hm21 <alex.frei@waio.ch>
2026-09-14 10:14:09 +0200

    test: drop an import the analyzer flags in the example

--- example/integration_test/error_handling_test.dart
+++ example/integration_test/error_handling_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:pro_video_editor/pro_video_editor.dart';
